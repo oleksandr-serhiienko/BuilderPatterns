d2e7ab68c538f71e8713796dad8d9f3712850756
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 17:15:52 +0200

    commited task

---
+++ Builder/Task.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coding.Exercise
+{
+
+    public class ClassRepresentation
+    {
+        public string Name { get; set; }
+        public List<FieldRepresentation> FieldRepresentations { get; set; } = new List<FieldRepresentation>();
+    }
+
+    public class FieldRepresentation
+    {
+        public string FieldName { get; set; }
+
+        public string TypeName { get; set; }
+    }
+
+
+
+    public class CodeBuilder
+    {
+        ClassRepresentation classRepresentation = new ClassRepresentation();
+
+        public CodeBuilder(string className)
+        {
+            
+            classRepresentation.Name = className;
+        }
+
+        public CodeBuilder AddField(string fieldName, string typeName)
+        {
+            FieldRepresentation fiedFieldRepresentation 
+                = new FieldRepresentation() 
+                    { FieldName = fieldName, TypeName = typeName};
+
+            classRepresentation.FieldRepresentations.Add(fiedFieldRepresentation);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"public class {classRepresentation.Name}");
+            sb.AppendLine("{");
+            foreach (var field in classRepresentation.FieldRepresentations)
+            {
+                sb.Append("\t").AppendLine($"public {field.TypeName} {field.FieldName};");
+            }
+            sb.AppendLine("}");
+            
+            return sb.ToString();
+        }
+
+        public static void Main()
+        {
+            var dd = new CodeBuilder("Check").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(dd);
+        }
+    }
+}
